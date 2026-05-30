7f97ab4dabe6bfd0e039da67e50c34da12cf89ed
danfornazeiro <danielacfornazeiro123@usp.br>
2026-05-30 11:18:57 -0300

    fix: fixing securityconfig for filter before, sessionmanagement, exceptionhandling

--- backend/src/main/java/br/com/vanroute/backend/config/SecurityConfig.java
+++ backend/src/main/java/br/com/vanroute/backend/config/SecurityConfig.java
@@ -1,24 +1,44 @@
 package br.com.vanroute.backend.config;
 
+import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticatorFilter filter;
+
+    public SecurityConfig(JwtAuthenticatorFilter filter) {
+        this.filter = filter;
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpStatus.FORBIDDEN.value());
+                        }))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
-
+                        .anyRequest().permitAll())
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
