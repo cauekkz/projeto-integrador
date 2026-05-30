d3db86931c16f2b006c8afe010110a11a67c84c7
danfornazeiro <danielacfornazeiro123@usp.br>
2026-05-30 10:59:37 -0300

    feat: add JwtAuthenticatorFilter and switch type of password

--- backend/src/main/java/br/com/vanroute/backend/config/SecurityConfig.java
+++ backend/src/main/java/br/com/vanroute/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,7 +24,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Argon2
-        return org.springframework.security.crypto.argon2.Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+        return new BCryptPasswordEncoder();
     }
 }
