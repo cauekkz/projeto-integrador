3163b16837a273afabeacb611d5615e91727a7f2
Gustavo Moura <viny.coelho30@gmail.com>
2026-08-17 16:08:47 -0300

    feat: input to write the student  code

--- frontend/src/app/components/student-code/student-code.ts
+++ frontend/src/app/components/student-code/student-code.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './student-code.html',
   styleUrl: './student-code.css',
 })
-export class StudentCode {}
+export class StudentCode {
+
+
+  onInput(event: Event, index: number) {
+    const input = event.target as HTMLInputElement;
+    input.classList.remove('animate');
+    void input.offsetWidth;
+    input.classList.add('animate');
+    if (input.value.length === 1) {
+      const inputs = document.querySelectorAll('.code-input');
+      const next = inputs[index + 1] as HTMLInputElement;
+      if (next) next.focus();
+    }
+
+  }
+
+
+  onKeydown(event: KeyboardEvent, index: number) {
+    const input = event.target as HTMLInputElement;
+    if (event.key === 'Backspace' && input.value === '') {
+      const inputs = document.querySelectorAll('.code-input');
+      const prev = inputs[index - 1] as HTMLInputElement;
+      if (prev) prev.focus();
+    }
+  }
+}
